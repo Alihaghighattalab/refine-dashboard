fd48f0f3f5eb1a44953a9e3deacadd79ff752b7a
alihtofficial <alihtofficial@gmail.com>
2024-01-29 19:01:23 +0330

    complete dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,31 @@
-import { GitHubBanner, Refine, WelcomePage } from "@refinedev/core";
+import {
+  Authenticated,
+  GitHubBanner,
+  Refine,
+  WelcomePage,
+} from "@refinedev/core";
 import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
 import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
 
 import { useNotificationProvider } from "@refinedev/antd";
 import "@refinedev/antd/dist/reset.css";
 
-import dataProvider, {
-  GraphQLClient,
-  liveProvider,
-} from "@refinedev/nestjs-query";
 import routerBindings, {
+  CatchAllNavigate,
   DocumentTitleHandler,
   UnsavedChangesNotifier,
 } from "@refinedev/react-router-v6";
 import { App as AntdApp } from "antd";
-import { createClient } from "graphql-ws";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
 import { authProvider } from "./authProvider";
 import { ColorModeContextProvider } from "./contexts/color-mode";
-
-const API_URL = "https://api.nestjs-query.refine.dev/graphql";
-const WS_URL = "wss://api.nestjs-query.refine.dev/graphql";
-
-const gqlClient = new GraphQLClient(API_URL);
-const wsClient = createClient({ url: WS_URL });
+import { DashboardPage } from "./components/dashboard";
+import { Login } from "./pages/login";
+import { Register } from "./pages/register";
+import { ForgotPassword } from "./pages/forgotPassword";
+import { resources } from "./config/resources";
+import { dataProvider, liveProvider } from "./providers";
+import { Layout } from "./components/layout";
 
 function App() {
   return (
@@ -34,11 +36,12 @@ function App() {
           <AntdApp>
             <DevtoolsProvider>
               <Refine
-                dataProvider={dataProvider(gqlClient)}
-                liveProvider={liveProvider(wsClient)}
+                dataProvider={dataProvider}
+                liveProvider={liveProvider}
                 notificationProvider={useNotificationProvider}
                 routerProvider={routerBindings}
                 authProvider={authProvider}
+                resources={resources}
                 options={{
                   syncWithLocation: true,
                   warnWhenUnsavedChanges: true,
@@ -48,7 +51,23 @@ function App() {
                 }}
               >
                 <Routes>
-                  <Route index element={<WelcomePage />} />
+                  <Route path="/register" element={<Register />} />
+                  <Route path="/login" element={<Login />} />
+                  <Route path="/forgot-password" element={<ForgotPassword />} />
+                  <Route
+                    element={
+                      <Authenticated
+                        key="authenticated-layout"
+                        fallback={<CatchAllNavigate to="/login" />}
+                      >
+                        <Layout>
+                          <Outlet />
+                        </Layout>
+                      </Authenticated>
+                    }
+                  >
+                    <Route index element={<DashboardPage />} />
+                  </Route>
                 </Routes>
                 <RefineKbar />
                 <UnsavedChangesNotifier />
---
+++ src/graphql/types.ts
@@ -0,0 +1,121 @@
+import type * as Types from "./schema.types";
+
+export type DashboardDealsChartQueryVariables = Types.Exact<{
+  filter: Types.DealStageFilter;
+  sorting?: Types.InputMaybe<Array<Types.DealStageSort> | Types.DealStageSort>;
+  paging?: Types.InputMaybe<Types.OffsetPaging>;
+}>;
+
+export type DashboardDealsChartQuery = {
+  dealStages: Pick<Types.DealStageConnection, "totalCount"> & {
+    nodes: Array<
+      Pick<Types.DealStage, "id" | "title"> & {
+        dealsAggregate: Array<{
+          groupBy?: Types.Maybe<
+            Pick<
+              Types.DealStageDealsAggregateGroupBy,
+              "closeDateMonth" | "closeDateYear"
+            >
+          >;
+          sum?: Types.Maybe<Pick<Types.DealStageDealsSumAggregate, "value">>;
+        }>;
+      }
+    >;
+  };
+};
+
+export type DashboardLatestActivitiesDealsQueryVariables = Types.Exact<{
+  filter: Types.DealFilter;
+  sorting?: Types.InputMaybe<Array<Types.DealSort> | Types.DealSort>;
+  paging?: Types.InputMaybe<Types.OffsetPaging>;
+}>;
+
+export type DashboardLatestActivitiesDealsQuery = {
+  deals: Pick<Types.DealConnection, "totalCount"> & {
+    nodes: Array<
+      Pick<Types.Deal, "id" | "title" | "createdAt"> & {
+        stage?: Types.Maybe<Pick<Types.DealStage, "id" | "title">>;
+        company: Pick<Types.Company, "id" | "name" | "avatarUrl">;
+      }
+    >;
+  };
+};
+
+export type DashboardLatestActivitiesAuditsQueryVariables = Types.Exact<{
+  filter: Types.AuditFilter;
+  sorting?: Types.InputMaybe<Array<Types.AuditSort> | Types.AuditSort>;
+  paging?: Types.InputMaybe<Types.OffsetPaging>;
+}>;
+
+export type DashboardLatestActivitiesAuditsQuery = {
+  audits: Pick<Types.AuditConnection, "totalCount"> & {
+    nodes: Array<
+      Pick<
+        Types.Audit,
+        "id" | "action" | "targetEntity" | "targetId" | "createdAt"
+      > & {
+        changes: Array<Pick<Types.AuditChange, "field" | "from" | "to">>;
+        user?: Types.Maybe<Pick<Types.User, "id" | "name" | "avatarUrl">>;
+      }
+    >;
+  };
+};
+
+export type DashboardCalendarUpcomingEventsQueryVariables = Types.Exact<{
+  filter: Types.EventFilter;
+  sorting?: Types.InputMaybe<Array<Types.EventSort> | Types.EventSort>;
+  paging: Types.OffsetPaging;
+}>;
+
+export type DashboardCalendarUpcomingEventsQuery = {
+  events: Pick<Types.EventConnection, "totalCount"> & {
+    nodes: Array<
+      Pick<Types.Event, "id" | "title" | "color" | "startDate" | "endDate">
+    >;
+  };
+};
+
+export type DashboardTotalCountsQueryVariables = Types.Exact<{
+  [key: string]: never;
+}>;
+
+export type DashboardTotalCountsQuery = {
+  companies: Pick<Types.CompanyConnection, "totalCount">;
+  contacts: Pick<Types.ContactConnection, "totalCount">;
+  deals: Pick<Types.DealConnection, "totalCount">;
+};
+
+export type UpdateUserMutationVariables = Types.Exact<{
+  input: Types.UpdateOneUserInput;
+}>;
+
+export type UpdateUserMutation = {
+  updateOneUser: Pick<
+    Types.User,
+    "id" | "name" | "avatarUrl" | "email" | "phone" | "jobTitle"
+  >;
+};
+
+export type UsersSelectQueryVariables = Types.Exact<{
+  filter: Types.UserFilter;
+  sorting?: Types.InputMaybe<Array<Types.UserSort> | Types.UserSort>;
+  paging: Types.OffsetPaging;
+}>;
+
+export type UsersSelectQuery = {
+  users: Pick<Types.UserConnection, "totalCount"> & {
+    nodes: Array<Pick<Types.User, "id" | "name" | "avatarUrl">>;
+  };
+};
+
+export type TaskStagesSelectQueryVariables = Types.Exact<{
+  filter: Types.TaskStageFilter;
+  sorting?: Types.InputMaybe<Array<Types.TaskStageSort> | Types.TaskStageSort>;
+  paging: Types.OffsetPaging;
+}>;
+
+export type TaskStagesSelectQuery = {
+  taskStages: Pick<Types.TaskStageConnection, "totalCount"> & {
+    nodes: Array<Pick<Types.TaskStage, "id" | "title">>;
+  };
+};
